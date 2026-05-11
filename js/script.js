e88a0484d6a3fd4e0e13b32dd16f72c6835f8174
Tarte23 <tarrhet11@ecolecatholique.ca>
2026-05-11 10:07:06 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,202 @@
-1
+ /*
+ var a100 = document.getElementById("a100").value;
+ var a200 = document.getElementById("a200").value;
+ var a300 = document.getElementById("a300").value;
+ var a400 = document.getElementById("a400").value;
+ var a500 = document.getElementById("a500").value;
+ 
+ var b100 = document.getElementById("b100").value;
+ var b200 = document.getElementById("b200").value;
+ var b300 = document.getElementById("b300").value;
+ var b400 = document.getElementById("b400").value;
+ var b500 = document.getElementById("b500").value;
+ 
+ var c100 = document.getElementById("c100").value;
+ var c200 = document.getElementById("c200").value;
+ var c300 = document.getElementById("c300").value;
+ var c400 = document.getElementById("c400").value;
+ var c500 = document.getElementById("c500").value;
+ 
+ var d100 = document.getElementById("d100").value;
+ var d200 = document.getElementById("d200").value;
+ var d300 = document.getElementById("d300").value;
+ var d400 = document.getElementById("d400").value;
+ var d500 = document.getElementById("d500").value;
+ 
+ var e100 = document.getElementById("e100").value;
+ var e200 = document.getElementById("e200").value;
+ var e300 = document.getElementById("e300").value;
+ var e400 = document.getElementById("e400").value;
+ var e500 = document.getElementById("e500").value; 
+ */
+ 
+ 
+ /* <button type = "button" button class="jeopardy-button" onclick="play()" id="a100">$100</button>
+    if (id ==='a100') {
+         
+         alert("a100 woorrrrksss");
+         
+     }
+ */
+ 
+ 
+ 
+ alert("Test fichier JS.");
+ 
+ function play() {
+     
+     var message = ""
+     if (id ==='a100') {
+         
+         message = "a100"
+         
+         
+     }
+     
+     else if (id ==='a200') {
+         
+         
+         
+     }
+     
+     else if (id ==='a300') {
+         
+         
+         
+     }
+     
+     else if (id ==='a400') {
+         
+         
+         
+     }
+     
+     else if (id ==='a500') {
+         
+         
+         
+     }
+     
+     else if (id ==='b100') {
+         
+         
+         
+     }
+     
+     else if (id ==='b200') {
+         
+         
+         
+     }
+     
+     else if (id ==='b300') {
+         
+         
+         
+     }
+     
+     else if (id ==='b400') {
+         
+         
+         
+     }
+     
+     else if (id ==='b500') {
+         
+         
+         
+     }
+     
+     else if (id ==='c100') {
+         
+         
+         
+     }
+     
+     else if (id ==='c200') {
+         
+         
+         
+     }
+     
+     else if (id ==='c300') {
+         
+         
+         
+     }
+     
+     else if (id ==='c400') {
+         
+         
+         
+     }
+     
+     else if (id ==='c500') {
+         
+         
+         
+     }
+     
+     else if (id ==='d100') {
+         
+         
+         
+     }
+     
+     else if (id ==='d200') {
+         
+         
+         
+     }
+     
+     else if (id ==='d300') {
+         
+         
+         
+     }
+     
+     else if (id ==='d400') {
+         
+         
+         
+     }
+     
+     else if (id ==='d500') {
+         
+         
+         
+     }
+     
+     else if (id ==='e100') {
+         
+         
+         
+     }
+     
+     else if (id ==='e200') {
+         
+         
+         
+     }
+     
+     else if (id ==='e300') {
+         
+         
+         
+     }
+     
+     else if (id ==='e400') {
+         
+         
+         
+     }
+     
+     else if (id ==='e500') {
+         
+         
+         
+     }
+     
+     affiche.innerHTML = message;
+     
+ }
